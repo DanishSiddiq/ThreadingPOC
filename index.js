e052d8ee15f1caebd08b1aee6d434a2b5af0da07
DanishSiddiq <mdanishsiddiq@yahoo.com>
2018-08-18 17:38:14 +0400

    Comments added for better clarifications

--- index.js
+++ index.js
@@ -1,57 +1,76 @@
+// module included to create worker threads
 const { Worker } = require('worker_threads');
 
 // main attributes
-let lst;
-let index = -1;
-let myWorker;
+let lst;    // list will be populated from 0 to n
+let index = -1; // index will be used to traverse list
+let myWorker; // worker reference
 let interval;
 
 mainBody();
 
 function mainBody () {
     console.log("Main Thread Started");
-    lst = Array(1e1).fill().map((v,i) => i);
+
+    // filling array with 100 items
+    lst = Array(1e2).fill().map((v,i) => i);
+
+    // initiating worker process
     initiateWorker();
+
+    // traversing list in main method with specific interval
     interval = setInterval(function(){ processDataInMainThread(); }, 1000);
 }
 
+// index and value of list will be consoled
 function processDataInMainThread() {
     // update index
     index++;
-    // now check first length
-    if( lst.length > index) {
+    // check first length
+    if ( lst.length > index) {
         console.log("Message from Main Thread at Index: ", index, " and value is: ", lst[index]);
     }
-    else{
+    // no further calling if all items are traversed
+    else {
         clearInterval(interval);
     }
 }
 
+// Defining callback method for receiving data or error on worker thread
 function initiateWorker () {
 
     // define callback
     let cb = (err, result) => {
         if(err) { return console.error(err); }
 
+        // if worker thread is still working on list then write index and updated value
         if(result.isInProgress) {
-            console.log("Message from child at Index: ", result.index, " and updated value: ", result.val);
+            console.log("Message from worker at Index: ", result.index, " and updated value: ", result.val);
         }
-        else{
+        // when worker thread complete process then console original list from main and updated list from worker thread
+        else {
             console.log("Original List Data: ", lst);
-            console.log("Updated List From Child: ", result.val);
+            console.log("Updated List From worker: ", result.val);
         }
     };
+
     // start worker
     myWorker = startWorker(__dirname + '/workerCode.js', cb);
-    // post a multiple factor to worker
-    myWorker.postMessage({ multipleFactor: getRandomArbitrary(3,9)});
+
+    // post a multiple factor to worker thread
+    myWorker.postMessage({ multipleFactor: getRandomArbitrary(3,9) });
 }
 
 function startWorker(path, cb) {
+    // sending path and data to worker thread constructor
     let w = new Worker(path, { workerData: lst });
+
+    // registering events in main thread to perform actions after receiving data/error/exit events
     w.on('message', (msg) => {
+        // data will be passed into callback
         cb(null, msg);
     });
+
     // for error handling
     w.on('error', cb);
 
@@ -64,6 +83,7 @@ function startWorker(path, cb) {
     return w;
 }
 
+// for generating a random number between min and max
 function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
--- workerCode.js
+++ workerCode.js
@@ -1,3 +1,5 @@
+// parentPort for registering to events from main thread
+// workerData for receiving data clone
 const {  parentPort, workerData } = require('worker_threads');
 
 let interval;
@@ -7,22 +9,24 @@ registerForEventListening();
 
 function registerForEventListening () {
 
+    // callback method is defined to receive data from main thread
     let cb = (err, result) => {
         if(err) return console.error(err);
 
         console.log("**** Multiple Factor Received From Parent Thread: ", result.multipleFactor, " ****");
 
-        //  setting up interval
+        //  setting up interval to call method to multiple with factor
         interval = setInterval(processDataAndSendData, 3000, result.multipleFactor);
     };
 
-    //you can receive messages from the main thread this way:
+    // registering to events to receive messages from the main thread
     parentPort.on('error', cb);
     parentPort.on('message', (msg) => {
         cb(null, msg);
     });
 }
 
+// item of list will be multiplied with a factor as per index
 function processDataAndSendData (multipleFactor) {
 
     // updating index
@@ -31,10 +35,10 @@ function processDataAndSendData (multipleFactor) {
     if( workerData.length > index) {
         // update value
         workerData[index] = workerData[index] * multipleFactor;
-        // send notification
+        // send updated value as notification along with in progress flag as true
         parentPort.postMessage({ index, val: workerData[index], isInProgress:true });
     } else {
-        // send notification
+        // send complete updated list as notification, when processing is done
         parentPort.postMessage({ val: workerData, isInProgress:false });
         clearInterval(interval);
     }
